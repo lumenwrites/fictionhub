89cb07bfcbe01e6eae9dadac7449d4d5d676e052
Ray Alez <raymestalez@gmail.com>
2017-06-09 21:32:26 -0700

    Update.

--- backend/posts/migrations/0006_auto_20170610_0214.py
+++ backend/posts/migrations/0006_auto_20170610_0214.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations, models
 import django.db.models.deletion
-
+from series.models import Series
 
 class Migration(migrations.Migration):
 
