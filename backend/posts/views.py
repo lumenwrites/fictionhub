78b0fd28990acd077fa357bfb8d0f6ad0db83930
Ray Alez <raymestalez@gmail.com>
2017-06-13 00:58:35 -0700

    Update.

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -257,9 +257,6 @@ class PostDetailView(DetailView):
         nested_comments = list(get_comment_list(ranked_comments, rankby="hot"))
         context['comments'] = nested_comments
 
-        # Profile (for header above single posts)
-        context['profile'] = post.author
-
         # Prev/next chapters
         if post.series:
             chapters = post.series.chapters.filter(published=True)
