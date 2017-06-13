2b198e925240a71a9bbb707088801243d28dd518
Ray Alez <raymestalez@gmail.com>
2017-06-12 23:31:07 -0700

    Update.

--- backend/static_serve/js/scripts.js
+++ backend/static_serve/js/scripts.js
@@ -70,14 +70,14 @@ $(document).ready(function() {
     }
 
     /* Google Analytics */
-    /* 
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-44003603-19', 'auto');
+
+    ga('create', 'UA-44003603-24', 'auto');
     ga('send', 'pageview');
-    */
+
     /* Tooltips */
     $('[data-toggle="tooltip"]').tooltip();
 
