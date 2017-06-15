c22fec24b344b81868891eea2230e9d00a1a7fa8
Ray Alez <raymestalez@gmail.com>
2017-06-15 04:10:34 -0700

    Update.

--- backend/series/urls.py
+++ backend/series/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import series_update, series_purchase
+from .views import series_update, series_purchase,purchase
 
 urlpatterns = [
     url(r'^series/(?P<slug>[^\.]+)/update$', series_update),    
     url(r'^series/(?P<slug>[^\.]+)/(?P<username>[^\.]+)/purchase$', series_purchase),
+    url(r'^purchase$', purchase),    
 ]
