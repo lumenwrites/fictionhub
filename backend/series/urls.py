377fd1d8925e69b80eddbfa24fdbadaa5644b33d
Ray Alez <raymestalez@gmail.com>
2017-06-15 04:27:49 -0700

    Update.

--- backend/series/urls.py
+++ backend/series/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
-from .views import series_update, series_purchase,purchase
+from .views import series_update, series_purchase
 
 urlpatterns = [
     url(r'^series/(?P<slug>[^\.]+)/update$', series_update),    
     url(r'^series/(?P<slug>[^\.]+)/(?P<username>[^\.]+)/purchase$', series_purchase),
-    url(r'^purchase$', purchase),    
 ]
--- backend/series/views.py
+++ backend/series/views.py
@@ -44,25 +44,3 @@ def series_purchase(request, slug, username):
     # Redirect to first paid chapter
     return HttpResponseRedirect(first_paid_chapter.get_absolute_url())
 
-
-# Trying to do the same from specified url
-@csrf_exempt
-def purchase(request):
-    # Grab data from POST request sent by paypal
-    slug = request.POST.get('item_name')
-    username = request.POST.get('username')
-    series = Series.objects.get(slug=slug)
-    user = User.objects.get(username=username)
-    # Add to user's purchased series
-    user.purchased_series.add(series)
-    user.save()
-
-    # Update author's balance
-    first_paid_chapter = series.chapters.all()[series.free_chapters]
-    author = first_paid_chapter.author
-    # Increment authors balance (minus $1 which goes to me)
-    author.balance += series.price - 1
-    author.save()
-
-    # Redirect to first paid chapter
-    return HttpResponseRedirect(first_paid_chapter.get_absolute_url())
